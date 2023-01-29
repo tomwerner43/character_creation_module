60d56a4f1a7b1c14f3cddd460dd59295259486e9
Денис Доценко <werner313@gmail.com>
2023-01-29 13:32:17 +0300

    Add Docstring code

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
@@ -98,4 +98,4 @@ def main():
     print(start_training(char_name, char_class))
 
 
-main()
+
